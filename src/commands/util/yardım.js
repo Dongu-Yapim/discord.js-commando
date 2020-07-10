06e791df31ffe3b8d2e93c876be9a7978c9af6d5
Multiplen <29270213+Ripbyrustavi02@users.noreply.github.com>
2020-07-10 08:54:24 +0300

    Update yardım.js

--- src/commands/util/yardım.js
+++ src/commands/util/yardım.js
@@ -36,19 +36,33 @@ module.exports = class HelpCommand extends Command {
 
 let group;
 var groups = this.client.registry.groups.map(g => g.id);
-
+if (!msg.channel.type === "dm") {
 const Discord = require("discord.js");
 const yardim = new Discord.RichEmbed()
 .setTitle("Çay Bot Komut Listesi")
 .addField("<:cay:731000115149668383>  +yardım çay", "Çayın temel olan çay iç ve benzeri komutlarını bulunduran komut listesini gösterir.", true)
 .addField("<:photoshop:731001860768399420>  +yardım efekt", "Avatarınıza verebileceğiniz harika efektler ve caspleri bulunduran komut listesini gösterir", true)
-.addField("<:moderation:731002423967219752>  +yardım admin", "Sunucunuzu daha iyi yönetebileceğiniz, admin ve moderatör komut listesini gösterir", true)
+.addField("<:moderation:731002423967219752>  +yardım yetkili", "Sunucunuzu daha iyi yönetebileceğiniz, admin ve moderatör komut listesini gösterir", true)
 .addField("<:eglence:731002417398677585>  +yardım eğlence", "Sunucunuz da hoş vakit geçirebileceğiniz eğlence komut listesini gösterir ", true)
 .addField("<:Upvoted:731002774237741116>  +yardım seviye", "Ayrıntılı seviye sisteminin çay içinde komut listesini gösterir", true)
 .addField("<:8187_crystal_cluster:731003873858420749>  +yardım genel", "Diğer kategorilerin dışında birbirinden farklı komut listesini gösterir", true)
 .setFooter("Çay ve dünyanın en iyi topluluğu " + msg.guild)
 .setColor("#f4c2c2")
+if (!args.command.toLowerCase()) return msg.say(yardim);  
+} else {
+const Discord = require("discord.js");
+const yardim = new Discord.RichEmbed()
+.setTitle("Çay Bot Komut Listesi")
+.addField("<:cay:731000115149668383>  +yardım çay", "Çayın temel olan çay iç ve benzeri komutlarını bulunduran komut listesini gösterir.", true)
+.addField("<:photoshop:731001860768399420>  +yardım efekt", "Avatarınıza verebileceğiniz harika efektler ve caspleri bulunduran komut listesini gösterir", true)
+.addField("<:moderation:731002423967219752>  +yardım yetkili", "Sunucunuzu daha iyi yönetebileceğiniz, admin ve moderatör komut listesini gösterir", true)
+.addField("<:eglence:731002417398677585>  +yardım eğlence", "Sunucunuz da hoş vakit geçirebileceğiniz eğlence komut listesini gösterir ", true)
+.addField("<:Upvoted:731002774237741116>  +yardım seviye", "Ayrıntılı seviye sisteminin çay içinde komut listesini gösterir", true)
+.addField("<:8187_crystal_cluster:731003873858420749>  +yardım genel", "Diğer kategorilerin dışında birbirinden farklı komut listesini gösterir", true)
+.setFooter("Çay ve dünyanın en mükemmel insanı " + msg.author.username)
+.setColor("#f4c2c2")
 if (!args.command.toLowerCase()) return msg.say(yardim);   
+}
 
 
 if (!groups.some(g => args.command.toLowerCase() == g)) return msg.channel.send(`Girdiğiniz komut yanlış, şu şekilde kullanacaksınız: **+yardım efekt**`, {embed: emb})
